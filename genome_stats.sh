a3d872a5f0d49d7bad9dfba6d9a7bcaf05607519
Chris Wheat <chriswheat@zoologi195.zoologi.su.se>
2020-05-28 11:01:49 +0200

    update

--- genome_stats.sh
+++ genome_stats.sh
@@ -55,4 +55,56 @@ Average length of break (>25 Ns) between contigs in scaffold        923
                                            contig %non-ACGTN       0.25
                                Number of contig non-ACGTN nt     716413
 
-						 
+perl /mnt/griffin/chrwhe/software/Assemblathon/assemblathon_stats.pl Colias_crocea_genome_V1_mespa_scaffolded.fa
+
+---------------- Information for assembly 'Colias_crocea_genome_V1_mespa_scaffolded.fa' ----------------
+
+
+                                         Number of scaffolds       7916
+                                     Total size of scaffolds  320036068
+                                            Longest scaffold    1007683
+                                           Shortest scaffold        952
+                                 Number of scaffolds > 1K nt       7909  99.9%
+                                Number of scaffolds > 10K nt       4203  53.1%
+                               Number of scaffolds > 100K nt        972  12.3%
+                                 Number of scaffolds > 1M nt          1   0.0%
+                                Number of scaffolds > 10M nt          0   0.0%
+                                          Mean scaffold size      40429
+                                        Median scaffold size      12082
+                                         N50 scaffold length     110365
+                                          L50 scaffold count        834
+                                                 scaffold %A      30.26
+                                                 scaffold %C      14.71
+                                                 scaffold %G      14.78
+                                                 scaffold %T      30.25
+                                                 scaffold %N       9.78
+                                         scaffold %non-ACGTN       0.22
+                             Number of scaffold non-ACGTN nt     716413
+
+                Percentage of assembly in scaffolded contigs      96.1%
+              Percentage of assembly in unscaffolded contigs       3.9%
+                      Average number of contigs per scaffold        5.3
+Average length of break (>25 Ns) between contigs in scaffold        909
+
+                                           Number of contigs      42323
+                              Number of contigs in scaffolds      39304
+                          Number of contigs not in scaffolds       3019
+                                       Total size of contigs  288755686
+                                              Longest contig     441266
+                                             Shortest contig         34
+                                   Number of contigs > 1K nt      39629  93.6%
+                                  Number of contigs > 10K nt       9224  21.8%
+                                 Number of contigs > 100K nt         10   0.0%
+                                   Number of contigs > 1M nt          0   0.0%
+                                  Number of contigs > 10M nt          0   0.0%
+                                            Mean contig size       6823
+                                          Median contig size       4230
+                                           N50 contig length      11958
+                                            L50 contig count       7120
+                                                   contig %A      33.53
+                                                   contig %C      16.31
+                                                   contig %G      16.38
+                                                   contig %T      33.53
+                                                   contig %N       0.01
+                                           contig %non-ACGTN       0.25
+                               Number of contig non-ACGTN nt     716413
